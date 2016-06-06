aa2aca258194b90d45c6c16b45a14fe0b5188980
Gijs Hendrickx <305857@student.fontys.nl>
2016-06-06 13:16:09 +0200

    Again

--- target/generated-sources/annotations/model/Account_.java
+++ target/generated-sources/annotations/model/Account_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-06T12:50:19")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-06-06T13:08:18")
 @StaticMetamodel(Account.class)
 public class Account_ { 
 
